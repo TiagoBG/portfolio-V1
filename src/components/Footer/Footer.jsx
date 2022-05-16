d16e981ebd7d1880084db91400418d17b54b72c6
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-05-15 20:51:52 -0500

    Setting the base of the project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <h5>Copyright Viresland All rights 2022</h5>
+    </footer>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li className="nav-item">Home</li>
+                <li className="nav-item">Projects</li>
+                <li className="nav-item">Contact</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
